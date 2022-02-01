8dca9759f05cbc790de51a4950d11dbd6791fa70
losfair <zhy20000919@hotmail.com>
2022-02-01 15:49:48 +0000

    Deploying to gh-pages from @ BlueLogicLabs/website@38665557790236d56520c88bcadb037f1e9031d3 🚀

---
+++ _next/static/S26ihQ-rsHGmpiE282p_t/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-03ab8ca502c5f863.js","static/css/6acd2e1b52d0fdd4.css","static/chunks/pages/index-6625e973d74b11c0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
